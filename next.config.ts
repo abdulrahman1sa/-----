c99b0e6333587ed801192d71f3556e6f296c0ab4
abdulrahman1sa <112275288+abdulrahman1sa@users.noreply.github.com>
2026-02-04 22:26:44 +0300

    Fix Bad Gateway by reverting to standard output

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  /* تفعيل وضع الـ standalone لتحسين الأداء في بيئات النشر */
-  output: 'standalone',
-  /* تجاهل الأخطاء البسيطة في الـ lint أثناء النشر لضمان استمرارية العملية */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  }
 };
 
 export default nextConfig;
